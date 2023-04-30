d9f576503067999646150e08df597d6232666f8a
gosapp <gosapps86contact@gmail.com>
2023-04-30 20:08:34 -0300

    agregue CORS, JWT, userById toma id del token

--- Template2/Program.cs
+++ Template2/Program.cs
@@ -4,47 +4,71 @@ using Infrastructure.Commands;
 using Infrastructure.Persistance;
 using Infrastructure.Queries;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
-using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(policy =>
+{
+    policy.AddDefaultPolicy(options => options.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Expreso de las diez - Microservicio de usuarios", Version = "v1" });
+
+    //Boton Autorize (Swagger)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Jwt Authorization",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
 });
 
 //Custom
-var connectionString = "";
-Console.WriteLine(Directory.GetCurrentDirectory());
-var gab = "C:\\Users\\Gabo\\Documents\\Backup\\unaj\\ProyectoDeSoftware_1\\2023-Primer-cuatri\\Grupal\\AppDeCitas\\UserMicroService2\\Template2\\Template2";
-var fran = @"C:\Users\LopezFranco\Desktop\Proyecto Sofware 2023\ExpressoDelasDiez\UserMicroService2\Template2\Template2";
+var connectionString = builder.Configuration["ConnectionString"];
 
-if(Directory.GetCurrentDirectory() == gab)
-{
-    connectionString =
-        builder.Configuration["ConnectionString2"];
-}
-if (Directory.GetCurrentDirectory() == fran)
-{
-    connectionString =
-        builder.Configuration["DefaultConnection"];
-}
-else 
-{
-    // MSSQL running locally
-    connectionString = builder.Configuration["ConnectionString"];
-}
-
-
-Console.WriteLine(connectionString);
 builder.Services.AddDbContext<ExpresoDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddTransient<IUserCommands, UserCommands>();
@@ -64,6 +88,7 @@ builder.Services.AddTransient<IServerImagesApiServices, ServerImagesApiServices>
 builder.Services.AddTransient<IAuthApiServices, AuthApiServices>();
 
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -74,6 +99,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
